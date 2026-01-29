2e801e0fad05b25a31cd7266dcfc7fcf3a6a7921
MasariuMan <masariuman@gmail.com>
2026-01-29 20:27:01 +0700

    meal-grid, meal-item

--- layout/meals/meals-grid.jsx
+++ layout/meals/meals-grid.jsx
@@ -1,7 +1,11 @@
-export default function MealsGrid({meals}) {
-  return <ul>
-    {meals.map(meal => <li key={meal.id}>
-      
-    </li>
-  </ul>
+import classes from "./meals-grid.module.css";
+
+export default function MealsGrid({ meals }) {
+  return (
+    <ul className={classes.meals}>
+      {meals.map((meal) => (
+        <li key={meal.id}></li>
+      ))}
+    </ul>
+  );
 }
