50ccd8d31ed1a3095e8581bea2639ac538b14eb6
MasariuMan <masariuman@gmail.com>
2026-01-29 20:23:13 +0700

    pagemeal & meals-grid

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>Meals Page</h1>    
-    </main>
-  )
+    <>
+      <header className={classes.header}>
+        <h1>
+          Gorengan Enak, Dibuat Oleh{" "}
+          <span className={classes.highlight}>Anda</span>
+        </h1>
+        <p>
+          Pilih resep kesukaanmu dan coba buat sendiri gorengannya. Resep sangat
+          mudah dan menyenangnkan!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Bagikan Resep Kesukaan Anda</Link>
+        </p>
+      </header>
+      <main className={classes.main}></main>
+    </>
+  );
 }
---
+++ layout/meals/meals-grid.jsx
@@ -0,0 +1,7 @@
+export default function MealsGrid({meals}) {
+  return <ul>
+    {meals.map(meal => <li key={meal.id}>
+      
+    </li>
+  </ul>
+}
