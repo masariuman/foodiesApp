344c77aeeae4153dbfb3aaba72e19dff980f9092
MasariuMan <masariuman@gmail.com>
2026-02-01 20:56:30 +0700

    .

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <main className="not-found">
       <h1>Halaman Tidak Dapat Ditemukan...</h1>
-      <p>Silahkan cek kembali alamat url yang dimasukkan...</p>
+      <p>Silahkan cek kembali alamat url yang dimasukkan....</p>
     </main>
   );
 }
