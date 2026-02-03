ad8a5125e7063de60beab292956528f8b4faae8b
MasariuMan <masariuman@gmail.com>
2026-02-03 20:51:13 +0700

    slug done

--- app/meals/[masariuman]/page.jsx
+++ app/meals/[masariuman]/page.jsx
@@ -1,26 +1,29 @@
 import Image from "next/image";
+import { getMeal } from "@/lib/meals";
 import classes from "./page.module.css";
 
-export default function MealDetailsPage() {
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.masariuman);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
   return (
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image fill />
+          <Image src={meal.image} alt={meal.title} fill />
         </div>
         <div className={classes.headerText}>
-          <h1>Title</h1>
+          <h1>{meal.title}</h1>
           <p className={classes.creator}>
-            by <a href={`mailto:${"EMAIL"}`}>Name</a>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
           </p>
-          <p className={classes.summary}>Summary</p>
+          <p className={classes.summary}>{meal.summary}</p>
         </div>
       </header>
       <main>
         <p
           className={classes.instructions}
           dangerouslySetInnerHTML={{
-            __html: "...",
+            __html: meal.instructions,
           }}
         ></p>
       </main>
