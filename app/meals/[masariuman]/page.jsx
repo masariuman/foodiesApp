fdbbf2871a97af5c16e20335fde4bdce09f21d4b
MasariuMan <masariuman@gmail.com>
2026-02-05 07:01:01 +0700

    pisah function getmeal di page

--- app/meals/[masariuman]/page.jsx
+++ app/meals/[masariuman]/page.jsx
@@ -3,31 +3,36 @@ import { getMeal } from "@/lib/meals";
 import classes from "./page.module.css";
 import { notFound } from "next/navigation";
 
-export default function MealDetailsPage({ params }) {
-  const meal = getMeal(params.masariuman);
+function Meal(id) {
+  const meal = getMeal(id.params.masariuman);
   if (!meal) {
     notFound();
   }
   meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+  return meal;
+}
+
+export default function MealDetailsPage({ params }) {
+  const data = Meal({ params });
   return (
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image src={meal.image} alt={meal.title} fill />
+          <Image src={data.image} alt={data.title} fill />
         </div>
         <div className={classes.headerText}>
-          <h1>{meal.title}</h1>
+          <h1>{data.title}</h1>
           <p className={classes.creator}>
-            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+            by <a href={`mailto:${data.creator_email}`}>{data.creator}</a>
           </p>
-          <p className={classes.summary}>{meal.summary}</p>
+          <p className={classes.summary}>{data.summary}</p>
         </div>
       </header>
       <main>
         <p
           className={classes.instructions}
           dangerouslySetInnerHTML={{
-            __html: meal.instructions,
+            __html: data.instructions,
           }}
         ></p>
       </main>
