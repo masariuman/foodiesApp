ce54501858f19c1d9837f4a8799ec19f9c7cf22f
MasariuMan <masariuman@gmail.com>
2026-02-05 13:04:19 +0700

    imagePicker Preview Image

--- layout/meals/image-picker.jsx
+++ layout/meals/image-picker.jsx
@@ -1,17 +1,38 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 
 export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState(null);
   const imageInput = useRef();
   function handlePickClick() {
     imageInput.current.click();
+  }
+  function handleImageChange(e) {
+    const file = event.target.files[0];
+    if (!file) {
+      return;
+    }
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
   }
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && (
+            <p>Pilih Gambar Terlebih Dahulu Dengan Tombol Upload Gambar.</p>
+          )}
+          {pickedImage && (
+            <Image src={pickedImage} alt="Gambar Yang Sudah Dipilih" fill />
+          )}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -19,6 +40,7 @@ export default function ImagePicker({ label, name }) {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInput}
+          onChange={handleImageChange}
         />
         <button
           className={classes.button}
@@ -31,3 +53,4 @@ export default function ImagePicker({ label, name }) {
     </div>
   );
 }
+//bisa multiple, 118 chapter awal next
