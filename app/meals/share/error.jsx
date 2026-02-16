6d1aad71792fbae05cb13e089ff2c071d7c2f218
MasariuMan <masariuman@gmail.com>
2026-02-16 21:28:24 +0700

    error share

---
+++ app/meals/share/error.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function error({ error }) {
+  return (
+    <main className="error">
+      <h1>Ada Error Bro!</h1>
+      <p>Inputan yang bro isi salah...</p>
+    </main>
+  );
+}
