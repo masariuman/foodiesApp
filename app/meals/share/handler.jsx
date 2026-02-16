4f3e3965505084301334db94624e08cc7b24869d
MasariuMan <masariuman@gmail.com>
2026-02-16 21:23:29 +0700

    handler validation

--- app/meals/share/handler.jsx
+++ app/meals/share/handler.jsx
@@ -6,3 +6,7 @@ export const shareMeal = async (formData) => {
   const data = store(formData);
   return data;
 };
+
+export const validation = (data) => {
+  return !data || data.trim() === "";
+};
